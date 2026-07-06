e23953d2b5ebf2f38f39d868648d82cdaaf20eb8
Paulo Ricardo <pauloricardoso.dev@gmail.com>
2026-07-06 13:21:32 -0300

    refactor: move controller logic to service layer

--- src/main/java/io/github/pauloricardo/springbootstudy/controller/StudentController.java
+++ src/main/java/io/github/pauloricardo/springbootstudy/controller/StudentController.java
@@ -22,16 +22,15 @@ public class StudentController {
     @GetMapping("/student")
     public Student getStudent() {
 
-        return new Student( 1, "Paulo", 25);
+        return studentService.getStudent();
 
     }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
 
-        return List.of (new Student( 1, "Paulo", 23),
-                        new Student( 2, "João", 26),
-                        new Student( 3, "Carlos", 27));
+        return studentService.getStudents();
+
     }
 
     @GetMapping("/students/{id}")
@@ -42,7 +41,9 @@ public class StudentController {
 
     @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
-        return student;
+
+        return studentService.createStudent(student);
+
     }
 
 }
--- src/main/java/io/github/pauloricardo/springbootstudy/service/StudentService.java
+++ src/main/java/io/github/pauloricardo/springbootstudy/service/StudentService.java
@@ -3,6 +3,8 @@ package io.github.pauloricardo.springbootstudy.service;
 import io.github.pauloricardo.springbootstudy.model.Student;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -10,6 +12,25 @@ public class StudentService {
 
         return new Student( id, "Aluno " + id, 20 + id);
 
+    }
+
+    public Student getStudent() {
+
+        return new Student( 1, "Paulo", 25);
+
+    }
+
+    public List<Student> getStudents() {
+
+        return List.of(new Student( 1, "Paulo", 25),
+                       new Student(2, "João", 26),
+                       new Student(3, "Maria", 27));
+
+    }
+
+    public Student createStudent(Student student) {
+
+        return student;
     }
 
 }
